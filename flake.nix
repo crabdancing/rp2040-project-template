b24d4e408a3dec3ba05d93ddb2ebbb64abe4fbef
Alexandria P. <alxpettit@gmail.com>
2024-04-11 23:17:16 -0600

    my failures are reproducible yayyyy

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Build a cargo project without extra checks";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
 
     crane = {
       url = "github:ipetkov/crane";
